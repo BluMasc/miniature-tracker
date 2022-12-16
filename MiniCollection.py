415516e2a293206960e1844d5fe6822223714e1b
BluMasc <blumasc@googlemail.com>
2022-12-16 13:32:16 +0100

    Made basic window layout

--- MiniCollection.py
+++ MiniCollection.py
@@ -2,78 +2,112 @@
 
 import PySimpleGUI as sg
 import os
-from googletrans import Translator
+import json
+from pathlib import Path
 
-trans = Translator(service_urls=['translate.googleapis.com'])
-layout = [  [sg.Text('Welche Worte sollen uebersetzt werden?')],
-            [sg.Input(key='-IN-', enable_events=True)],
+p = Path(__file__).with_name('config.json')
 
-            [sg.Button('TRANSLATE', bind_return_key=True)],
+with p.open('r') as f:
+  cfg = json.load(f)
 
-            [sg.Text("Ajawo' Script:"),sg.Text("",font=["Ajawo Script", 18],key='_AJAWO_',size=(20,1))],
+values = [str(i) for i in range(20)]
 
-            [sg.Button('Discord Copy')]
+chooseColumn = [  [sg.Text('Models')],
+            [sg.Input(key='-IN-', enable_events=True), sg.Button('Search', bind_return_key=True), sg.Button('Help')],
+            [sg.Listbox(
+                values=values,
+                select_mode=sg.LISTBOX_SELECT_MODE_SINGLE,
+                enable_events=True,
+                size=(60, 20),
+                key="list"),
+                sg.Column("")],
 
             ]
+editAddon = [
+            [sg.Button('Edit') ,sg.Button('Add'),sg.Button('Delete')]
+            ]
+
+if cfg["Edit Mode"]:
+    chooseColumn = chooseColumn+editAddon
+
+infoColumn = [
+    [sg.Text('Model', key='-text-',font=("",20))],
+    [sg.Image(key="-IMAGE-")],
+    [sg.Column([[sg.Text('Source',font="bold"),sg.Text('-', key='-source-')],
+        [sg.Text('Link',font="bold"),sg.Text('-', key='-link-')],
+        [sg.Text('Price',font="bold"),sg.Text('-', key='-price-')],
+        [sg.Text('Painted By',font="bold"),sg.Text('-', key='-painted-')],
+        [sg.Text('Status',font="bold"),sg.Text('-', key='-status-')],
+        [sg.Text('Material',font="bold"),sg.Text('-', key='-material-')],
+        [sg.Text('Location',font="bold"),sg.Text('-', key='-location-')]]),
+    sg.Column([[sg.Text('Enviroment',font="bold"),sg.Text('-', key='-enviroment-')],
+        [sg.Text('Plane',font="bold"),sg.Text('-', key='-plane-')],
+        [sg.Text('Size',font="bold"),sg.Text('-', key='-size-')],
+        [sg.Text('Type',font="bold"),sg.Text('-', key='-type-')],
+        [sg.Text('Class',font="bold"),sg.Text('-', key='-class-')],
+        [sg.Text('Attacks',font="bold"),sg.Text('-', key='-atk-')],
+        [sg.Text('Tags',font="bold"),sg.Text('-', key='-tag-')]])],
+
+
+]
+
+layout = [
+    [
+        sg.Column(chooseColumn),
+        sg.VSeperator(),
+        sg.Column(infoColumn, element_justification='c'),
+    ]
+]
+
+searchIndicators =[["","Name (: is default comperator)"],["so","Source"],["li","link"],["pr","price"]]
+inHeaders = ["Indicator","Atribute"]
+searchComperators=[[":","contains"],["=","is equal to"],["~","is equal to regex"],["#","does not contain"]]
+coHeaders = ["Comperators", "Function"]
 
 # Create the window
-window = sg.Window("Ajawo' Translator", layout)
+window = sg.Window("Mini Tracker", layout)
+def open_help_window():
+    helplayout = [
+        [
+            sg.Text('Help',font=("",20))
+        ],
+        [
+            sg.Text('If you want to search by name you can just enter the text that the name should contain')
+        ],
+        [
+            sg.Text('(not using ";" or any comparison symbol).')
+        ],
+        [
+            sg.Text('You can specify search parameters by writing an indicator followed by a symbol for comparison.')
+        ],
+        [
+            sg.Text('Each parameter is seperated by a ";". ')
+        ],
+        [sg.Column(
+        [[
+            sg.Text('Indicators',font=("",15))
+        ],
+        [
+            sg.Table(values=searchIndicators, headings=inHeaders)
+        ]]),
+        sg.VSeperator(),
+        sg.Column(
+        [[
+            sg.Text('Comperators',font=("",15))
+        ],
+        [
+            sg.Table(values=searchComperators, headings=coHeaders)
+        ]])
+        ]
+    ]
+    helpwindow = sg.Window("Mini Tracker Help", helplayout, modal=True)
+    choice = None
+    while True:
+        event, values = helpwindow.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
 
-def translate ():
-    ui = values["-IN-"]
-
-    uis = str(ui)
-
-    tui = trans.translate(uis)
-    print(tui.text)
-    window.Element('_AJAWO_').Update(tui.text)
-
-def copytoclip ():
-    ui = values["-IN-"]
-    uis = str(ui)
-    tui = trans.translate(uis)
-    print(tui.text)
-    window.Element('_AJAWO_').Update(tui.text)
-    char_to_replace = {
-                        ' ':"    ",
-                        'a':":AjawoHouse: ",
-                        'b':":AjawoWave: ",
-                        'c':":AjawoSwoosh: ",
-                        'd':":AjawoPincer: ",
-                        'e':":AjawoHorns: ",
-                        'f':":AjawoSnake: ",
-                        'g':":AjawoIron: ",
-                        'h':":AjawoBow: ",
-                        'i':":AjawoCandle: ",
-                        'j':":AjawoBird: ",
-                        'k':":AjawoFaucet: ",
-                        'l':":AjawoParalel: ",
-                        'm':":AjawoSerpent: ",
-                        'n':":AjawoCyclon: ",
-                        'o':":AjawoScythe: ",
-                        'p':":AjawoTree: ",
-                        'q':":AjawoCrown: ",
-                        'r':":AjawoChair: ",
-                        's':":AjawoCup: ",
-                        't':":AjawoBoat: ",
-                        'u':":AjawoFling: ",
-                        'v':":AjawoKick: ",
-                        'w':":AjawoGrab: ",
-                        'x':":AjawoDagger: ",
-                        'y':":AjawoHook: ",
-                        'z':":AjawoClam: ",
-                        '.':":AjawoCircle: ",
-                        ',':":AjawoLocation: ",
-                        "'":":AjawoReverseLocation: ",
-                        '!':":AjawoScratch: ",
-                        '?':":AjawoThreeway: ",
-                        ';':":AjawoRaindrop: ",
-                        ':':":AjawoHalfcircle: ",
-                        '"':":AjawoFairy: "
-                        }
-    out = tui.text.lower().translate(str.maketrans(char_to_replace))
-    command = 'echo ' + out.strip() + '| clip'
-    os.system(command)
+    helpwindow.close()
 
 # Create an event loop
 while True:
@@ -82,8 +116,8 @@ while True:
     # presses the OK button
     if event == sg.WIN_CLOSED or event == "EXIT":
         break
-    elif event == "TRANSLATE":
-        translate()
+    elif event == "Help":
+        open_help_window()
     elif event == "Discord Copy":
         copytoclip()
     window.refresh()
